a2f1c19e4bd6b81c04e1828a37b706633315c8db
dinithaw <ldinitha@gmail.com>
2025-01-12 22:56:32 +0530

    modified

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,34 +1,14 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-
-//     mongoose.connection.on('connected',() => {
-//         console.log("DB Connected");
-//     })
-
-//     await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-
-// }
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("DB Connected");
-  });
 
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Successfully connected to MongoDB!");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1); // Exit the process with failure
-  }
-};
+    mongoose.connection.on('connected',() => {
+        console.log("DB Connected");
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+
+}
 
 export default connectDB;
+
